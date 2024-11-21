085f2364d92142f15decab6ead112bef79e17e48
Shubham Gupta <shubhamguptaji81@gmail.com>
2024-11-21 20:10:16 +0530

    Removed duplicate code

--- src/src/repositories/channelRepostiory.js
+++
@@ -1,8 +0,0 @@
-import Channel from '../schema/channel.js';
-import crudRepository from './crudRepository.js';
-
-const channelRepository = {
-  ...crudRepository(Channel)
-};
-
-export default channelRepository;
