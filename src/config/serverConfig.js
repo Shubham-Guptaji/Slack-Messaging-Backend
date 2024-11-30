bc6ee69b2f3a89ce6e273ec4399f57707eeb9ecc
Shubham Gupta <shubhamguptaji81@gmail.com>
2024-11-30 12:55:11 +0530

    added password in redis

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -21,3 +21,5 @@ export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
 export const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 export const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
+
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD || 'undefined';
