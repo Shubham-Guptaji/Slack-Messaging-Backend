d30534681efac6445048deca969a47ee937798a3
Shubham Gupta <shubhamguptaji81@gmail.com>
2024-11-30 10:12:26 +0530

    Added Socket.io for websocket and two way communication

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
+import { Server } from 'socket.io';
 
+import bullServerAdapter from './config/bullBoardConfig.js';
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import messageHandlers from './controllers/messageSocketController.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/ui', bullServerAdapter.getRouter());
 
 app.use('/api', apiRouter);
 
@@ -16,7 +24,18 @@ app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  // console.log('a user connected', socket.id);
+
+  // socket.on('messageFromClient', (data) => {
+  //   console.log('Message from client', data);
+
+  //   io.emit('new message', data.toUpperCase()); // broasdcast
+  // });
+  messageHandlers(io, socket);
+});
+
+server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
--- src/routes/v1/workspaces.js
+++ src/routes/v1/workspaces.js
@@ -1,8 +1,21 @@
 import express from 'express';
 
-import { createWorkspaceController } from '../../controllers/workspaceController.js';
+import {
+  addChannelToWorkspaceController,
+  addMemberToWorkspaceController,
+  createWorkspaceController,
+  deleteWorkspaceController,
+  getWorkspaceByJoinCodeController,
+  getWorkspaceController,
+  getWorkspacesUserIsMemberOfController,
+  updateWorkspaceController
+} from '../../controllers/workspaceController.js';
 import { isAuthenticated } from '../../middlewares/authMiddleware.js';
-import { createWorkspaceSchema } from '../../validators/workspaceSchema.js';
+import {
+  addChannelToWorkspaceSchema,
+  addMemberToWorkspaceSchema,
+  createWorkspaceSchema
+} from '../../validators/workspaceSchema.js';
 import { validate } from '../../validators/zodValidator.js';
 
 const router = express.Router();
@@ -13,4 +26,33 @@ router.post(
   validate(createWorkspaceSchema),
   createWorkspaceController
 );
+
+router.get('/', isAuthenticated, getWorkspacesUserIsMemberOfController);
+
+router.delete('/:workspaceId', isAuthenticated, deleteWorkspaceController);
+
+router.get('/:workspaceId', isAuthenticated, getWorkspaceController);
+
+router.get(
+  '/join/:joinCode',
+  isAuthenticated,
+  getWorkspaceByJoinCodeController
+);
+
+router.put('/:workspaceId', isAuthenticated, updateWorkspaceController);
+
+router.put(
+  '/:workspaceId/members',
+  isAuthenticated,
+  validate(addMemberToWorkspaceSchema),
+  addMemberToWorkspaceController
+);
+
+router.put(
+  '/:workspaceId/channels',
+  isAuthenticated,
+  validate(addChannelToWorkspaceSchema),
+  addChannelToWorkspaceController
+);
+
 export default router;
